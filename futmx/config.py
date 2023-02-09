36c8d972b1d16109dea1ad1afc86f4c97a551f66
denstream-io <dennislc.prog@gmail.com>
2023-02-09 14:39:27 +0000

    first commit

---
+++ futmx/config.py
@@ -0,0 +1,44 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__)) ## Use pathlib
+
+class Config():
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'c8f8b09584c19ad3957beb3d80983ba7'
+    # MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    # MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # FLASK_MAIL_SUBJECT_PREFIX = '[CleverB]'
+    # FLASKY_MAIL_SENDER = ...
+    # FLASK_ADMIN = os.environ.get('FLASK_ADMIN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'test-data.sqlite')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+config = {
+    'development': DevelopmentConfig,
+    'testing':TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
+
+    
+
+ 
